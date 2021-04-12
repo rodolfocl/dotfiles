bc628330cf9fdee925b1220f49fa954de5077a29
rodolfovenegas <rodolfovenegas.es@gmail.com>
2021-04-11 22:28:49 -0400

    CONFIGGLOBAL: Agrego alias para trabajar con webstorm desde consola y subo cambios en la configuracion de iterm

--- shell/_aliases/global.sh
+++ shell/_aliases/global.sh
@@ -8,10 +8,13 @@ alias dotfiles="cd /Users/rodolfovenegas/.dotfiles/"
 alias opendot="open .dotfiles"
 
 # Open directories with finder, intellij or vscode
+alias wstorm="open -na "WebStorm.app""
+alias w="wstorm"
+alias w.="wstorm ."
 alias idea="/usr/local/bin/idea"
 alias i="idea"
-alias c="code"
 alias i.="idea ."
+alias c="code"
 alias c.="code ."
 alias o.="open ."
 
