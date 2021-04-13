839e81f613d62c0cd9e2516c3f510a2808aa7d4e
rodolfovenegas <rodolfovenegas.es@gmail.com>
2021-04-13 15:02:56 -0400

    FUNCIONES: Creo archivo global, para definir variables y que el resto de las funciones tengan acceso

---
+++ shell/_functions/global.sh
@@ -0,0 +1,17 @@
+# VARIABLES GLOBALES
+# PARA QUE LAS FUNCIONES TENGAN ACCESO A ELLAS
+
+# Color texto
+RED='\033[0;31m'
+DGRAY='\033[1;30m'
+YELLOW='\033[1;33m'
+LCYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
+# Background color texto
+B_WHITE='\e[0;47m'
+B_NC='\e[0m' # No Color
+
+# Formato texto
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
