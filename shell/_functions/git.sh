be20fb78734609ba58e8dae2a72d89ea11497fff
rodolfovenegas <rodolfovenegas.es@gmail.com>
2021-04-13 15:28:18 -0400

    FUNCIONES: Agrego tres nuevas funciones de git

--- shell/_functions/git.sh
+++ shell/_functions/git.sh
@@ -3,8 +3,9 @@ source /Users/rodolfovenegas/.dotfiles/shell/_functions/global.sh
 
 
 function pretty-diff() {
-  # Ver git diff con un previsualizador de los cambios en la terminal
-  # Utilizando fzf: bre install fzf
+  ##? Ver git diff con un previsualizador de los cambios en la terminal
+  ##? Utilizando fzf: bre install fzf
+  ##?  1.0.0
   ##?
   ##? Usage:
   ##? pretty-diff, al dar enter en un archivomodificado copia el path del archivo
@@ -46,4 +47,38 @@ function pretty-log() {
   commit_hash=$(echo ${commit} | awk '{print $2}')
 
   echo ${commit_hash} | tr -d '\n' | pbcopy
+}
+
+function show-ignored() {
+  ##? Show all ignored files
+  #?? 1.0.0
+  ##?
+  ##? Usage:
+  ##? show-ignored
+
+  git status --ignored -s | grep "\!\!"
+}
+
+
+function find-msg-commit() {
+  ##? Find commits by commit message
+  #?? 1.0.0
+  ##?
+  ##? Usage:
+  ##? find "message to find"
+
+  git log --pretty=format:'%C(yellow)%h  %Cblue%ad  %Creset%s%Cgreen  [%cn] %Cred%d' --decorate --date=short --regexp-ignore-case --grep=$1
+}
+
+
+function standup() {
+  ##? Muestra utltimos commit realizados por dias con su fecha y respectiva hora
+  #?? 1.0.0
+  ##?
+  ##? Usage:
+  ##? standup
+
+  docs::eval "$@"
+
+  git log --reverse --branches --since=$(if [[ "Mon" == "$(date +%a)" ]]; then echo "last friday"; else echo "yesterday"; fi) --author=$(git config --get user.email) --format=format:'%C(cyan) %ad %C(yellow)%h %Creset %s %Cgreen%d' --date=local
 }
