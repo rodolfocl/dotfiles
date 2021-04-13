9a7f96f6765e0fe488f0a622ef1c2ff251a56a3b
rodolfovenegas <rodolfovenegas.es@gmail.com>
2021-04-13 15:04:20 -0400

    FUNCIONES: Importo archivo global en git.sh

--- shell/_functions/git.sh
+++ shell/_functions/git.sh
@@ -1,19 +1,4 @@
-# VARIABLES GLOBALES
-
-# Color texto
-RED='\033[0;31m'
-DGRAY='\033[1;30m'
-YELLOW='\033[1;33m'
-LCYAN='\033[1;36m'
-NC='\033[0m' # No Color
-
-# Background color texto
-B_WHITE='\e[0;47m'
-B_NC='\e[0m' # No Color
-
-# Formato texto
-bold=$(tput bold)
-normal=$(tput sgr0)
+source /Users/rodolfovenegas/.dotfiles/shell/_functions/global.sh
 
 
 function pretty-diff() {
