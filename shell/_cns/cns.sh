ad02c7befa59ca356eadb900ef14b4e5774d4681
rodolfovenegas <rodolfovenegas.es@gmail.com>
2023-07-08 19:34:32 -0400

    SH: Agrego archivo sh para configuraciones de consorcio

---
+++ shell/_cns/cns.sh
@@ -0,0 +1,24 @@
+#================================================================
+# IMPORTO VARIABLES GLOBALES
+source /Users/rodolfovenegas/.dotfiles/shell/_global/variables.sh
+#================================================================
+
+# CONSORCIO
+
+# Node versions
+alias 18="nvm use 18.16.1"
+alias 19="nvm use 19.2.0"
+
+
+# Generales
+alias host="sudo vim /private/etc/hosts"
+
+
+# directories
+alias vida='cd Documents/cns/ms-vida-denuncios'
+alias dens='cd Documents/cns/densiniestro'
+alias ris='cd Documents/cns/desivi-3'
+
+
+# Execute projects
+alias go='npm run start'
