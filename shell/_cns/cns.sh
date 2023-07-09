d31f3e1ee0af07c9dd4b32a86ee31b167a6c1ab6
rodolfovenegas <rodolfovenegas.es@gmail.com>
2023-07-08 20:08:17 -0400

    SH: Agrego alias cns

--- shell/_cns/cns.sh
+++ shell/_cns/cns.sh
@@ -13,8 +13,8 @@ alias 19="nvm use 19.2.0"
 # Generales
 alias host="sudo vim /private/etc/hosts"
 
-
 # directories
+alias cns='cd Documents/cns'
 alias vida='cd Documents/cns/ms-vida-denuncios'
 alias dens='cd Documents/cns/densiniestro'
 alias ris='cd Documents/cns/desivi-3'
