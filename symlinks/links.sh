ca5382b12163750251d08709f035fc38546aa792
rodolfovenegas <rodolfovenegas.es@gmail.com>
2023-07-08 22:04:48 -0400

    SYMLINKS: Ordeno instrucciones para confoigurar framework

--- symlinks/links.sh
+++ symlinks/links.sh
@@ -1,34 +1,27 @@
+# =======================================================================================
+# DESDE LA RAIZ DEL PROYECTO
+# =======================================================================================
 
-# EJECUTAR DESDE EL HOME /USERS/RODOLFOVENEGAS PARA EJECUTAR TODOS LOS ENLACES SIMBOLICOS DE UNA VEZ:
-#
+# EJECUTAR DESDE EL HOME /USERS/RODOLFOVENEGAS
 # sh .dotfiles/symlinks/links.sh
 
-#EL PROBLEMA DE EJECUTARLO ASI ES QUE LOS ENLACES SE GUARDARAN EN EL DIRECTORIO SYMLINKS, LO MEJOR
-#ES EJECUTARLO MANUALMENTE LOS LINKS
 
-#sh .dotfiles/symlinks/links.sh in .dotfiles/git
+# =======================================================================================
+# PASOS A EJECUTAR
+# =======================================================================================
 
-# Eliminar un enlace simbolico: se debe pasar el nombre del enlace a unlink, dicho nombre es el
-# nombre del archivo del cual se esta creando el link simbolico ejemplo: ".gitconfig -> .dotfiles/git/.gitconfig"
-#
-# unlink ./.gitconfig
+# Si el archivo no existe en .dotfiles se debe mover archivos dentro del proyecto.
+# Si ya existe eliminar el archivo de la raiz del proyecto, se creara un ¡no nuevo con link simbolico.
 
-
-# =======PASOS A EJECUTAR=======
-# Mover archivos dentro de los dotfiles o Eliminar el archivo en la raiz del proyecto, ya que al existir en los dotfiles se hace solo el enlace simbolico, no debe existir para poder crearlo:
 # MOVER:
 # mv .gitconfig .dotfiles/git
+
 # ELIMINAR:
 # Seleccionar archivo y eliminar
 
-# Crear link simbolico
-# ln -s .dotfiles/git/.gitconfig
-
-# ver todos los archivos y los con emlaces simbolicos
-# ls -la
-
-# ver solo los archivos que tengan enlace simbolico
-# ls -la | grep "\->"
+# =======================================================================================
+# CREAR LINKS SIMBOLICOS
+# =======================================================================================
 
 # Listados de links simbolicos
 # ruta donde esta el archivo " " donde estaba antes el archivo
@@ -50,10 +43,13 @@ ln -s .dotfiles/git/.gitignore_global $PWD/.gitignore_global
 
 # Karabiner-elements
 # mv .config/karabiner/karabiner.json .dotfiles/so/mac/karabiner-elements/
-ln -s .dotfiles/so/mac/karabiner-elements/karabiner.json $PWD/.config/karabiner/karabiner.json
+#ln -s .dotfiles/so/mac/karabiner-elements/karabiner.json $PWD/.config/karabiner/karabiner.json
 
 
 # VSCODE
 #ln -s .dotfiles/editors/vs-code/settings.json $PWD/Library/Application\ Support/code/user/settings.json
 #ln -s .dotfiles/editors/vs-code/keybindings.json $PWD/Library/Application\ Support/code/user/keybindings.json
 #ln -s .dotfiles/editors/vs-code/snippets $PWD/Library/Application\ Support/code/user/snippets
+
+# ver solo los archivos que tengan enlace simbolico
+# ls -la | grep "\->"
