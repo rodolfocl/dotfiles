ef475e8effac8f1bfbcd64fb6e68d01bb04117a9
rodolfovenegas <rodolfovenegas.es@gmail.com>
2024-03-21 16:07:41 -0300

    ZSH: Configuro alias documatico

--- shell/_cns/cns.sh
+++ shell/_cns/cns.sh
@@ -9,18 +9,22 @@ source /Users/rodolfovenegas/.dotfiles/shell/_global/variables.sh
 alias 18="nvm use 18.16.1"
 alias 19="nvm use 19.2.0"
 
-
 # Generales
 alias host="sudo vim /private/etc/hosts"
 
-# directories
+# Directories CONSORCIO
 alias cns='cd Documents/cns'
+alias ris='cd Documents/cns/desivi-3'
+alias dens='cd Documents/cns/densiniestro'
+alias sf='cd Documents/cns/usr_salesforce'
+
+#  MS y API vida-denuncios
 alias vida='cd Documents/cns/vida-denuncios/ms-vida-denuncios'
-alias dens='cd Documents/cns/vida-denuncios/densiniestro'
-alias ris='cd Documents/cns/vida-denuncios/desivi-3'
 alias facade='cd Documents/cns/vida-denuncios/ms-salesforce-facade'
 alias apiv='cd Documents/cns/vida-denuncios/api-vida-denuncios'
 
+# Directories Documatico
+alias doc='cd Documents/cns/documatico/ms-core-vida-documatico'
 
 # Execute projects
 alias go='npm run start:dev'
